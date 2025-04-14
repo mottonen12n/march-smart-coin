2c4858ab748e1cdff3d04601455b5624a025929a
mottonen12n <mottonen12n@gmail.com>
2025-04-14 11:41:47 -0700

    Finished generation and output for each division of bracket and Final Four. Cleaned up readme a little bit.

--- README.md
+++ README.md
@@ -7,10 +7,9 @@ Added rules are as follows:
   * 1/16 chance for 16 to beat 1 seed.
   * 1/8 chance for 15 to beat 2 seed.
   * 1/4 chance for 14 to beat 3 seed and for 13 to beat 4 seed.
-  * 1/4 chance for 8/9 to beat 1 seed (if 1 seed survives 1st round).
+  * 1/4 chance for 8/9 to beat 1 seed (if 1 seed survives 1st round, otherwise a 1/2 between 8/9 and 16).
   * All other matches are 1/2 for each seed to win.
   
 
-Initial thought on output of this will be a string of 1's and 0's with 0 representing the team on the top of the matchup winning as viewed when looking at the bracket (not necessarily the top seeded team).
+Output of this will be a string of 1's and 0's with 0 representing the team on the top of the matchup winning as viewed when looking at the bracket (not necessarily the top seeded team).
 There would then be 4 of these strings with 15 numbers each, one for each section/division of the bracket. Then a final 3 number string for the Final Four and Championship.
-(If this changes I'll totally remember to update this).
--- main.py
+++ main.py
@@ -10,16 +10,33 @@ def main_menu():
             selection = input("1: Generate Bracket Picks\n0: Exit\n")
             match selection:
                 case "1":
-                    print(generatePicks())
+                    picks = generatePicks()
+                    print("South Bracket Picks to Final Four: " + picks[0])
+                    print("East Bracket Picks to Final Four: " + picks[1])
+                    print("West Bracket Picks to Final Four: " + picks[2])
+                    print("Midwest Bracket Picks to Final Four: " + picks[3])
+                    print("Final Four Picks: " + picks[4])
                 case "0":
                     loop = False
                 case _:
                     print("Invalid input, please try again.")
     except(Exception):
-        traceback.print_exc(file=sys.stdout)
+        #traceback.print_exc(file=sys.stdout)
         print("Error occured, try again.")
 
 def generatePicks():
+    result = []
+    
+    #Each division bracket up to Final Four
+    result.append(generateDivisionToFinalFour())
+    result.append(generateDivisionToFinalFour())
+    result.append(generateDivisionToFinalFour())
+    result.append(generateDivisionToFinalFour())
+    #Final Four
+    result.append(onein2()+onein2()+onein2())
+    return result
+
+def generateDivisionToFinalFour():
     result = ""
     #First Round
     #1-16
@@ -40,19 +57,21 @@ def generatePicks():
     result += onein8()
 
     #Second Round to Elite Eight
+    #1/16 vs. 8/9
     if(result[0] == "0"):
         result += onein4()
     else:
         result += onein2()
 
+    #Rest of Round 2
     result += onein2()
     result += onein2()
     result += onein2()
+    #Sweet Sixteen
     result += onein2()
     result += onein2()
+    #Elite Eight
     result += onein2()
-    
-    #Final Four
 
     return result
 
